58c7413c1bfbf24e2d2b36a0cc9218fba58114b1
Kevin Poretti <kevinporetti@gmail.com>
2016-09-14 21:58:25 -0400

    Move backups/autosave to separate folder

--- init.el
+++ init.el
@@ -74,6 +74,19 @@
     (ding)))
 (setq ring-bell-function 'my-bell-function) 
 
+; Move autosaves out of source directories
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+
 ; Line numbers
 (global-linum-mode t)
 
